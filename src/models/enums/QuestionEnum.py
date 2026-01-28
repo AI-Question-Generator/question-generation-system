a40d0a5b8f85adc69eda3275fcf7f0afd1735703
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-01-28 12:17:42 +0200

    Modified QuestionEnum

--- src/models/enums/QuestionEnum.py
+++ src/models/enums/QuestionEnum.py
@@ -1,8 +1,10 @@
 from enum import Enum
 
 class QuestionEnum(Enum):
-    ...
+    TRUE_FALSE = 'tf'
+    MULTIPLE_CHOICE = 'mcq'
+    SHORT_ANSWER = 'short_answer'
 
-class TrueOrFalseEnum(QuestionEnum):
+class TrueOrFalseEnum(Enum):
     TRUE = "True"
     FALSE = "False"
