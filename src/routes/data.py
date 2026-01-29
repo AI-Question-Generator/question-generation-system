088255aa9e3193304381704192cbc775c605caae
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-01-29 05:45:13 +0200

    Add Endpoint For uploading files to application.

---
+++ src/routes/data.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+from helpers import get_setting, Settings
+from controllers import DataController, ProjectController
+from models import ResponseSignal
+import os
+import aiofiles
+import logging
+
+logger = logging.getLogger('uvicorn.error')
+
+
+data_router = APIRouter(
+  prefix="/data",
+  tags=["data"]
+)
+
+@data_router.post("/upload/{project_id}")
+async def upload(project_id: str, file: UploadFile,
+                settings : Settings = Depends(get_setting)):
+  # validate file type & validate file size 
+  
+  data_controller = DataController()
+  
+  is_valid, result_signal = data_controller.validate_uploaded_file(file=file)
+  
+  if not is_valid:
+    return JSONResponse(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      content={
+        "signal": ResponseSignal.FILE_UPLOAD_FAILED.value}
+    )
+    
+  # get project path
+  project_dir_path = ProjectController().get_project_path(project_id=project_id)
+  
+  file_path = data_controller.generate_unique_filename(orig_file_name=file.filename,
+                                                        project_id=project_id)
+  
+  try:
+    
+    async with aiofiles.open(file_path, "wb") as f:
+      while chunk := await file.read(settings.FILE_DEFAULT_CHUNK_SIZE):
+        await f.write(chunk)
+        
+  except Exception as e:
+    
+    logger.error(f"Error While Uploading File: {e}")
+    
+    return JSONResponse(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      content={
+        "signal": ResponseSignal.FILE_UPLOAD_FAILED.value
+          }
+    )
+    
+  
+  return JSONResponse(
+      content={
+        "signal": ResponseSignal.FILE_UPLOADED_SUCCESSFULLY.value
+          }
+    )
