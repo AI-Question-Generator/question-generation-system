0238cb14a97eb343275ea5a51ed336565932d194
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-01-29 06:42:07 +0200

    update function for generate unique filepath.

--- src/controllers/DataController.py
+++ src/controllers/DataController.py
@@ -22,7 +22,7 @@ class DataController(BaseController):
     return True, ResponseSignal.FILE_UPLOADED_SUCCESSFULLY.value
   
   
-  def generate_unique_filename(self, orig_file_name: str, project_id: str):
+  def generate_unique_filepath(self, orig_file_name: str, project_id: str):
     
     random_filename = self.generate_random_string()
     project_path = ProjectController().get_project_path(project_id=project_id)
@@ -34,10 +34,10 @@ class DataController(BaseController):
                               )
     
     if os.path.exists(new_file_path):
-      return self.generate_unique_filename(orig_file_name=orig_file_name,
+      return self.generate_unique_filepath(orig_file_name=orig_file_name,
                                           project_id=project_id)  
     
-    return new_file_path
+    return new_file_path, random_filename+"_"+cleaned_filename
     
     
   def get_cleaned_filename(self, orig_file_name: str):
