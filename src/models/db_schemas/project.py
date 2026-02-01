a9e76f7dbe8dd6569962ae767ea9d216f33ed259
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-02-01 17:17:16 +0200

    Set Indecies for Projects & DataChunks.

--- src/models/db_schemas/project.py
+++ src/models/db_schemas/project.py
@@ -22,3 +22,13 @@ class Project(BaseModel):
     json_encoders = {
       ObjectId: str
     }
+    
+  @classmethod
+  def get_indexes(cls):
+    return [
+      {
+        "key":[("project_id", 1)],
+        "name": "project_id_index_1",
+        "unique": True
+      }
+    ]
