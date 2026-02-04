8c5afaa878b94a30a8035fc8345ddf91b461722b
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-02-04 09:23:11 +0200

    Add Response Signals in Enum.

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -6,5 +6,8 @@ class ResponseSignal(Enum):
   FILE_UPLOADED_SUCCESSFULLY = "file uploaded successfully"
   FILE_UPLOAD_FAILED = "file upload failed"
   
-  CHUNKING_COMPLETED = "chunking completed successfully"
+  CHUNKING_COMPLETED = "chunking process completed successfully"
   CHUNKING_FAILED = "chunking failed"
+  
+  NO_FILES_IDS = "No Files Founded"
+  FILE_ID_ERROR = "No File Founded With This ID"
