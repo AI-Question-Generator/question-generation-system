6ee1553703a0d070995edb9d8658d97a25fc5ac1
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-02-09 21:34:30 +0200

    Add some constant in ResponseSignal Enums.

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -11,3 +11,13 @@ class ResponseSignal(Enum):
   
   NO_FILES_IDS = "No Files Founded"
   FILE_ID_ERROR = "No File Founded With This ID"
+  
+  PROJECT_NOT_FOUND = "Project With This ID Not Found"
+  
+  INSERT_INTO_VECTOR_DB_ERROR = "Insert Into Vector DB Error"
+  INSERT_INTO_VECTOR_DB_SUCCESS = "Insert Into Vector DB Succeed"
+  
+  VECTOR_DB_COLLECTION_RETRIEVED = "VectorDB Collection Retrieved"
+  
+  VECTOR_DB_SEARCH_ERROR = "Vector DB Search Error"
+  VECTOR_DB_SEARCH_SUCCESS = "Vector DB Search Succeed"
