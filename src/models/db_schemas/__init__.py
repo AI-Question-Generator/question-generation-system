b0505ce6fe0fbe27f24556c59b9e3a763bcf60cb
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-02-11 21:38:57 +0200

    hotfix: removed MainIdeaChunk import in the __init__ file

--- src/models/db_schemas/__init__.py
+++ src/models/db_schemas/__init__.py
@@ -2,4 +2,3 @@ from .project import Project
 from .data_chunk import DataChunk, RetrievedDocument
 from .asset import Asset
 from .main_idea import MainIdea
-# from .main_idea_chunk import MainIdeaChunk
