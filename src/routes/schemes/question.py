18e7d5f988487325f045cc82cfd5197278896f64
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-02-15 22:25:48 +0200

    feat: renamed Question to BaseQuestion Model

--- src/routes/schemes/question.py
+++ src/routes/schemes/question.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 from typing import List, Literal
 from models.enums.QuestionEnum import TrueOrFalseEnum
 
-class Question(BaseModel):
+class BaseQuestion(BaseModel):
     question_statement: str
     explanation: str
     
-class MCQ(Question):
+class MCQ(BaseQuestion):
     correct_answer: str
     plausible_distractors: List[str]
 
-class TrueOrFalse(Question):
+class TrueOrFalse(BaseQuestion):
     correct_answer: Literal[TrueOrFalseEnum.TRUE.value, TrueOrFalseEnum.FALSE.value] # type: ignore
 
-class ShortAnswer(Question):
+class ShortAnswer(BaseQuestion):
     correct_answer: str
