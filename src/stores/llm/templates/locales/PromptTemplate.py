ec397a71de6a83acff91df620eac8eb7894bae90
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-02-27 21:37:37 +0200

    refactor: add Pydantic ConfigDict to allow Template types in PromptTemplate

--- src/stores/llm/templates/locales/PromptTemplate.py
+++ src/stores/llm/templates/locales/PromptTemplate.py
@@ -1,8 +1,11 @@
 from string import Template
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Any, Union
 
 class PromptTemplate(BaseModel):
+  
+  model_config = ConfigDict(arbitrary_types_allowed=True)
+  
   system: Union[Template, str]
   user: Union[Template, str]
   response_model: Any
