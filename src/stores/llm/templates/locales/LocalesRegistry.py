53375aaaa57e42b692c9c36ecbcedbd6c2a9ad45
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-02-27 21:37:26 +0200

    refactor: update LocalesRegistry with dynamic enum implementation

--- src/stores/llm/templates/locales/LocalesRegistry.py
+++ src/stores/llm/templates/locales/LocalesRegistry.py
@@ -19,5 +19,8 @@ def get_supported_domains(language: str) -> list[str]:
         if os.path.isdir(os.path.join(lang_path, name))
     ]
 
-class SupportedLanguage(str, Enum):
-    locals().update(_get_supported_languages())
+# class SupportedLanguage(str, Enum):
+#     locals().update(_get_supported_languages())
+    
+_langs = _get_supported_languages()
+SupportedLanguage = Enum('SupportedLanguage', _langs)
