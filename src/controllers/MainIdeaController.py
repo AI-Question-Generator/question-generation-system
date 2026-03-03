8aa42fdf70cfdeb98d59b31f45e318bf433eb506
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-03-03 19:06:14 +0200

    refactor: update MainIdeaController to fit with savaal router

--- src/controllers/MainIdeaController.py
+++ src/controllers/MainIdeaController.py
@@ -1,13 +1,10 @@
 from .BaseController import BaseController
-from models.db_schemas import MainIdea
-from typing import List, Dict, Tuple
-from models.MainIdeaModel import MainIdeaModel
-from bson.objectid import ObjectId
-from datetime import datetime, timezone
+from typing import List
 from stores.llm.templates.template_parser import PromptTemplateParserInterface
 import stores.llm.templates.response_models as rm
 from stores.llm.LLMInterface import AsyncLLMInterface
-from stores.vectordb.VectorDBInterface import VectorDBInterface
+from helpers.json_tools import pydantic_model_from_json
+
 import asyncio
 import logging
 import re
@@ -35,138 +32,116 @@ class MainIdeaController(BaseController):
     self._extraction_cache = {}  # Cache for extracted ideas
         
     
-  async def extract_candidates_from_sections(self, sections: List[str]) -> List[rm.MainIdea]:
+  async def extract_candidates_from_sections(self, sections: List[str]) -> List[str]:
 
     # Check cache first
-    section_key = tuple(sections)
-    if section_key in self._extraction_cache:
-      logger.info("Using cached extraction results")
-      return self._extraction_cache[section_key]
     
-    tasks = []
+    results = []
     for section in sections:
-      prompt = self.prompt_template_parser.get(
+      system_message, user_message, response_model = self.prompt_template_parser.get(
         "main_idea",
-        "extraction_prompt",
+        "extract_prompt",
         {"context": section}
       )
-      
-      if not prompt:
+            
+      if not system_message or not user_message:
         logger.error("Extraction prompt template not found")
         return []
       
-      if not isinstance(prompt.user, str) or not isinstance(prompt.system, str):
-        logger.error("Invalid prompt template format for main idea extraction")
-        return []
+      if not isinstance(user_message, str) or not isinstance(system_message, str):
+          logger.error("Invalid prompt template format for main idea extraction")
+          return []
       
       chat_history = [
         self.generation_client.construct_prompt(
-          prompt=prompt.system,
+          prompt=system_message,
           role=self.generation_client.enums.SYSTEM.value,
         )
       ]
-      
-      task = self.generation_client.generate_structured_text(
-        prompt=prompt.user, 
-        chat_history=chat_history, 
-        response_model=prompt.response_model
+      response = self.generation_client.generate_text(
+        prompt=user_message,
+        chat_history=chat_history,
       )
-      tasks.append(task)
+      results.append(response)
     
-    results = await asyncio.gather(*tasks, return_exceptions=True)
-    
-    main_ideas_list = []
-    for res in results:
-      if isinstance(res, BaseException):
-        logger.error(f"Exception occured when extracting idea from section: {res}")
-        continue
-      
-      res = prompt.response_model.model_validate_json(res)
-      main_ideas_list += list(res)
-    
-    # Cache the results
-    self._extraction_cache[section_key] = main_ideas_list
-    return main_ideas_list
+    return results
   
-  async def combine_candidates(self, candidates: List[rm.MainIdea]) -> List[rm.MainIdea]:
+  async def combine_candidates(self, candidates: list[str]) -> List[rm.MainIdea]:
     
     if not candidates:
       logger.error("No main idea candidates provided to combine")
       return []
     
-    context = "\n".join([
-      candidate.summary
-      for candidate in candidates
-    ])
+    context = "\n\n".join(candidates)
     
-    prompt = self.prompt_template_parser.get(
+    system_message, user_message, response_model = self.prompt_template_parser.get(
         "main_idea",
-        "combining_prompt",
+        "combine_prompt",
         {"context": context}
     )
     
-    if not prompt:
-      logger.error("Reducing prompt template not found")
+    if not system_message or not user_message:
+      logger.error("Combining prompt template not found")
       return []
     
-    if not isinstance(prompt.user, str) or not isinstance(prompt.system, str):
+    if not isinstance(user_message, str) or not isinstance(system_message, str):
         logger.error("Invalid prompt template format for main idea combining")
         return []
     
     chat_history = [
       self.generation_client.construct_prompt(
-        prompt=prompt.system,
+        prompt=system_message,
         role=self.generation_client.enums.SYSTEM.value,
       )
     ]
     
-    response = await self.generation_client.generate_structured_text(
-      prompt=prompt.user,
+    response = self.generation_client.generate_structured_text(
+      prompt=user_message,
       chat_history=chat_history,
-      response_model=prompt.response_model,
+      response_model=response_model
     )
-    
-    return list(prompt.response_model.model_validate_json(response)) if response else []
+
+    response = pydantic_model_from_json(response, response_model)
+    return list(response) if response else []
   
-  async def reduce_candidates(self, candidates: List[rm.MainIdea]) -> List[rm.MainIdea]:
+  async def reduce_candidates(self, candidates: List[rm.MainIdea], limit: int) -> List[rm.MainIdea]:
     
     if not candidates:
       logger.error("No main idea candidates provided to combine")
       return []
     
-    context = "\n".join([
-      candidate.summary
-      for candidate in candidates
-    ])
+    context = candidates
     
-    prompt = self.prompt_template_parser.get(
+    system_message, user_message, response_model = self.prompt_template_parser.get(
         "main_idea",
-        "combining_prompt",
-        {"context": context}
+        "reduce_prompt",
+        {"limit": limit, "context": context}
     )
     
-    if not prompt:
+    if not system_message or not user_message:
       logger.error("Reducing prompt template not found")
       return []
     
-    if not isinstance(prompt.user, str) or not isinstance(prompt.system, str):
+    if not isinstance(user_message, str) or not isinstance(system_message, str):
       logger.error("Invalid prompt template format for main idea reduction")
       return []
-      
+        
     chat_history = [
       self.generation_client.construct_prompt(
-        prompt=prompt.system,
+        prompt=system_message,
         role=self.generation_client.enums.SYSTEM.value,
       )
     ]
     
-    response = await self.generation_client.generate_structured_text(
-      prompt=prompt.user,
+    response = self.generation_client.generate_structured_text(
+      prompt=user_message,
       chat_history=chat_history,
-      response_model=prompt.response_model,
+      response_model=response_model
     )
+    
+    response = pydantic_model_from_json(response, response_model)
         
-    return list(prompt.response_model.model_validate_json(response)) if response else []
+    return list(response) if response else []
   
 
   async def rank_main_ideas(self, ideas: List[rm.MainIdea]) -> List[int]:
@@ -179,27 +154,27 @@ class MainIdeaController(BaseController):
       for idea in ideas
     ])
     
-    prompt = self.prompt_template_parser.get(
+    system_message, user_message, response_model = self.prompt_template_parser.get(
         "main_idea",
-        "ranking_prompt",
+        "rank_prompt",
         {"context": context}
     )
     
-    if not prompt:
+    if not system_message or not user_message:
       logger.error("Ranking prompt template not found")
       return []
     
     chat_history = [
       self.generation_client.construct_prompt(
-        prompt=prompt.system,
+        prompt=system_message,
         role=self.generation_client.enums.SYSTEM.value,
       )
     ]
     
-    response = await self.generation_client.generate_structured_text(
-      prompt=prompt.user,
+    response = self.generation_client.generate_text(
+      prompt=user_message,
       chat_history=chat_history,
-      response_model=prompt.response_model,
     )
     
-    return list(prompt.response_model.model_validate_json(response)) if response else []
+    response = pydantic_model_from_json(response, response_model)
+    return list(response) if response else []
