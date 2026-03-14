b041bfeed82b8eb41fdca95591ae16e49901fc99
Ahmed  Eltokhey <ahmdeltoky4@gmail.com>
2026-03-14 04:30:09 +0200

    feat: add async LLM provider factory implementation

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -31,3 +31,36 @@ class LLMProviderFactory:
       )
 
     return None
+  
+
+class AsyncLLMProviderFactory:
+  
+  def __init__(self, config: dict):
+    self.config = config
+    
+    def create(self, provider: str):
+    
+      if provider == LLMEnums.OPENAI.value:
+        return AsyncOpenAIProvider(
+          api_key=self.config.OPENAI_API_KEY,
+          api_url=self.config.OPENAI_API_URL,
+          default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+          default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+        )
+      
+      if provider == LLMEnums.COHERE.value:
+        return AsyncCoHereProvider(
+          api_key=self.config.COHERE_API_KEY,
+          default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+          default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+        )
+      
+      if provider == LLMEnums.GEMINI.value:
+        return AsyncGeminiProvider(
+          api_key= self.config.GEMINI_API_KEY,
+          default_input_max_characters= self.config.DEFAULT_INPUT_MAX_CHARACHTERS,
+          default_generation_max_output_tokens=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+          default_generation_temperature=self.config.GENERATION_DEFAULT_TEMPERATURE
+        )
+
+    return None
