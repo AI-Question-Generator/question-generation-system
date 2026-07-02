edf6ea68b1ef13c15e4ab14e8ab4b4efa05aa9ee
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-07-02 12:47:13 +0300

    fix: update return type annotations for json_repair_loads and pydantic_model_from_json functions

--- src/helpers/json_tools.py
+++ src/helpers/json_tools.py
@@ -1,11 +1,10 @@
 import json_repair
-from typing import Optional
 from pydantic import BaseModel
 
-def json_repair_loads(json_string, schema=None, salvage=True) -> Optional[dict]:
+def json_repair_loads(json_string, schema=None, salvage=True) -> dict:
   repaired_json = json_repair.loads(json_string, schema=schema, schema_repair_mode='salvage' if salvage else 'standard')
   return repaired_json
 
-def pydantic_model_from_json(json_string, model_class, salvage=True) -> Optional[BaseModel]:
+def pydantic_model_from_json(json_string, model_class, salvage=True) -> BaseModel:
   repaired_json = json_repair_loads(json_string, schema=model_class, salvage=salvage)
   return model_class.model_validate(repaired_json)
