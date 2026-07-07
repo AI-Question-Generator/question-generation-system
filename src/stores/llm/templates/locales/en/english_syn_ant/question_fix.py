60c3497bc7eaf0ded9a5e6154977286f7e263825
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-07-07 19:21:52 +0300

    feat: added english synonyms & antonyms prompt template

---
+++ src/stores/llm/templates/locales/en/english_syn_ant/question_fix.py
@@ -0,0 +1,5 @@
+from stores.llm.templates.locales.en import question_fix
+#### QUESTION GENERATION PROMPTS. ####
+
+# Multiple Choice Question Generation 
+question_fix_prompt = question_fix.question_fix_prompt
