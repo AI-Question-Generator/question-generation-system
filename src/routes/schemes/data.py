c3d4f5ca4980564f915fa2b1419c976e93090e05
Ahmed Elhefnawy <ahmedelhefnawy2003@yahoo.com>
2026-07-08 07:32:57 +0300

    fix: update ProcessRequest model to use Optional for file_id and adjust default chunk_size

--- src/routes/schemes/data.py
+++ src/routes/schemes/data.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ProcessRequest(BaseModel):
-  file_id : str = None
-  chunk_size: Optional[int] = 100
+  file_id : Optional[str] = None
+  chunk_size: Optional[int] = 200
   overlap_size: Optional[int] = 30
   do_reset: Optional[int] = 0
   
